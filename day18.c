5bbf9517aa8bac002c81fad9491b50eea0d0408b
its-kalpanachaudhary <kc3031673@gmail.com>
2026-02-18 14:18:14 +0530

    Add files via upload

---
+++ day18.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int maxSum = nums[0];
+    int currentSum = nums[0];
+
+    for(int i = 1; i < n; i++) {
+        if(currentSum + nums[i] > nums[i])
+            currentSum = currentSum + nums[i];
+        else
+            currentSum = nums[i];
+
+        if(currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    printf("%d", maxSum);
+
+    return 0;
+}
