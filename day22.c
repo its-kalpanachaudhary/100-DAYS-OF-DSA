a6900d8f3ebff8a40faf6a0b3f8f2ec3093209e6
its-kalpanachaudhary <kc3031673@gmail.com>
2026-02-24 09:37:36 +0530

    Add files via upload

---
+++ day22.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = temp->next;
+        }
+    }
+
+    // Count nodes
+    int count = 0;
+    temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
