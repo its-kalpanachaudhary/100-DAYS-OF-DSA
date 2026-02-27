f6b3bec50ae268821a739bfa856094dccaf039d4
its-kalpanachaudhary <kc3031673@gmail.com>
2026-02-27 15:42:54 +0530

    Add files via upload

---
+++ day27.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = temp->next;
+        }
+    }
+    return head;
+}
+
+// Get length of list
+int getLength(struct Node* head) {
+    int len = 0;
+    while (head != NULL) {
+        len++;
+        head = head->next;
+    }
+    return len;
+}
+
+// Find intersection node
+struct Node* findIntersection(struct Node* head1, struct Node* head2) {
+    int len1 = getLength(head1);
+    int len2 = getLength(head2);
+
+    int diff = abs(len1 - len2);
+
+    // Move pointer of longer list ahead
+    if (len1 > len2) {
+        while (diff--) head1 = head1->next;
+    } else {
+        while (diff--) head2 = head2->next;
+    }
+
+    // Traverse both together
+    while (head1 != NULL && head2 != NULL) {
+        if (head1->data == head2->data) {
+            return head1;  // intersection by value
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+
+    return NULL;
+}
+
+int main() {
+    int n, m;
+
+    scanf("%d", &n);
+    struct Node* list1 = createList(n);
+
+    scanf("%d", &m);
+    struct Node* list2 = createList(m);
+
+    struct Node* intersection = findIntersection(list1, list2);
+
+    if (intersection != NULL)
+        printf("%d", intersection->data);
+    else
+        printf("No Intersection");
+
+    return 0;
+}
